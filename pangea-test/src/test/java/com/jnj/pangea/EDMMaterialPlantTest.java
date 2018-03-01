13267fbe399cbebd2a793f534272e82757408d71
szhang54 <szhang54@its.jnj.com>
2018-03-01 20:04:44 +0800

    AEAZ-510 E.2.1.21 EDMMaterialPlant

---
+++ pangea-test/src/test/java/com/jnj/pangea/EDMMaterialPlantTest.java
@@ -0,0 +1,14 @@
+package com.jnj.pangea;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/test/resources/features/edm_material_plant.feature"},
+        tags = {"@pangea_test"},
+        plugin = {"pretty", "html:target/Destination"})
+public class EDMMaterialPlantTest {
+
+}
---
+++ pangea-view/src/main/java/com/jnj/pangea/view/bo/EDMMaterialPlantBo.java
@@ -0,0 +1,79 @@
+package com.jnj.pangea.view.bo;
+
+import com.jnj.adf.client.api.remote.RawDataHelper;
+
+public class EDMMaterialPlantBo extends BaseBo{
+
+    private String sourceSystem;
+    private String localMaterialNumber;
+    private String localPlant;
+    private String materialNumber;
+    private String plant;
+    private String localPlantStatus;
+    private String plantStatus;
+
+    public String getSourceSystem() {
+        return sourceSystem;
+    }
+
+    public void setSourceSystem(String sourceSystem) {
+        this.sourceSystem = sourceSystem;
+    }
+
+    public String getLocalMaterialNumber() {
+        return localMaterialNumber;
+    }
+
+    public void setLocalMaterialNumber(String localMaterialNumber) {
+        this.localMaterialNumber = localMaterialNumber;
+    }
+
+    public String getLocalPlant() {
+        return localPlant;
+    }
+
+    public void setLocalPlant(String localPlant) {
+        this.localPlant = localPlant;
+    }
+
+    public String getMaterialNumber() {
+        return materialNumber;
+    }
+
+    public void setMaterialNumber(String materialNumber) {
+        this.materialNumber = materialNumber;
+    }
+
+    public String getPlant() {
+        return plant;
+    }
+
+    public void setPlant(String plant) {
+        this.plant = plant;
+    }
+
+    public String getLocalPlantStatus() {
+        return localPlantStatus;
+    }
+
+    public void setLocalPlantStatus(String localPlantStatus) {
+        this.localPlantStatus = localPlantStatus;
+    }
+
+    public String getPlantStatus() {
+        return plantStatus;
+    }
+
+    public void setPlantStatus(String plantStatus) {
+        this.plantStatus = plantStatus;
+    }
+
+    @Override
+    public String getKey() {
+        return RawDataHelper.getInstance()
+                .makeJsonObject("sourceSystem", this.sourceSystem)
+                .add("localMaterialNumber",this.localMaterialNumber)
+                .add("localPlant",this.localPlant)
+                .toJsonString();
+    }
+}
