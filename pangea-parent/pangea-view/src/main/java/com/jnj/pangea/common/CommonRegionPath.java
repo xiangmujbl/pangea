c5cea81195818bd7dcc70e2698aa29e00fb94577
szhang54 <szhang54@its.jnj.com>
2018-03-07 15:27:08 +0800

    update CommonRegionPath

--- pangea-parent/pangea-view/src/main/java/com/jnj/pangea/common/CommonRegionPath.java
+++ pangea-parent/pangea-view/src/main/java/com/jnj/pangea/common/CommonRegionPath.java
@@ -4,10 +4,20 @@ package com.jnj.pangea.common;
  * Created by XZhan290 on 2018/3/6.
  */
 public interface CommonRegionPath {
-
+    // -----region
     String EDM_SOURCE_SYSTEM_V1 = "/edm/source_system_v1";
     String EDM_MATERIAL_GLOBAL_V1 = "/edm/material_global_v1";
     String PROJECT_ONE_MAKT = "/project_one/makt";
     String NGEMS_MATERIAL_LINKAGE = "/ngems/material_linkage";
     String NGEMS_GOLDEN_MATERIAL = "/ngems/golden_material";
+
+    // -----region attribute
+    String LOCALSOURCESYSTEM="localSourceSystem";
+
+    // -----region attribute value
+    // localSourceSystem="project_one"
+    String LOCALSOURCESYSTEM_PROJECT_ONE="project_one";
+
+    // z_source_system = [EMS]
+    String ZSOURCESYSTEM_EMS = "[EMS]";
 }
