258d9e6699d273198a90c56dc74e975f67a15367
XFu22 <XFu22@its.jnj.com>
2018-03-20 17:56:07 +0800

    update BaseFlowTest.java

--- pangea-parent/pangea-view/src/main/java/com/jnj/pangea/edm/mat_plant_fi/controller/EDMMatPlantFiController.java
+++ pangea-parent/pangea-view/src/main/java/com/jnj/pangea/edm/mat_plant_fi/controller/EDMMatPlantFiController.java
@@ -1,13 +1,13 @@
 package com.jnj.pangea.edm.mat_plant_fi.controller;
 
 import com.jnj.adf.curation.logic.RawDataEvent;
-import com.jnj.pangea.common.CommonController;
 import com.jnj.pangea.common.ResultObject;
+import com.jnj.pangea.common.controller.CommonController;
 import com.jnj.pangea.common.entity.projectone.MbewEntity;
 import com.jnj.pangea.edm.mat_plant_fi.service.EDMMatPlantFiServiceImpl;
 import com.jnj.pangea.util.BeanUtil;
 
-public class EDMMatPlantFiController extends CommonController{
+public class EDMMatPlantFiController extends CommonController {
 
     EDMMatPlantFiServiceImpl matPlantFiService = new EDMMatPlantFiServiceImpl();
 
