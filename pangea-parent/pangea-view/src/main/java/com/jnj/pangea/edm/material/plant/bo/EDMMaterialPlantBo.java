a025eac022d1b130453c76f626b44d64ccd8bb25
mchen <mchen>
2018-03-22 18:43:17 +0800

    material plant V2

--- pangea-parent/pangea-view/src/main/java/com/jnj/pangea/edm/material/plant/bo/EDMMaterialPlantBo.java
+++ pangea-parent/pangea-view/src/main/java/com/jnj/pangea/edm/material/plant/bo/EDMMaterialPlantBo.java
@@ -8,10 +8,308 @@ public class EDMMaterialPlantBo  extends BaseBo
     private String sourceSystem;
     private String localMaterialNumber;
     private String localPlant;
+    private String localDeletionFlagPlant;
     private String materialNumber;
     private String plant;
     private String localPlantStatus;
     private String plantStatus;
+    private String localProcurementType;
+    private String localFixedlotsize;
+    private String localMaximumLotSize;
+    private String localMinimumLotSize;
+    private String localRoundingvalueForPoq;
+    private String localLotsize;
+    private String localMrpType;
+    private String localMrpController;
+    private String localInHouseProcessingTime;
+    private String localSafetyStock;
+    private String localMinimumSafetyStock;
+    private String localProductionSupervisor;
+    private String localProductionUnit;
+    private String localPosttoInspectionStock;
+    private String localComponentScrapInPercent;
+    private String localCriticalpart;
+    private String localAbcIndicator;
+    private String localMaximumStocklevel;
+    private String localCheckingGroupforAvailabilityCheck;
+    private String localPlannedDeliveryTimeInDays;
+    private String localDependentRequirements;
+    private String localSafetytimeIndicator;
+    private String localSafetyTime;
+    private String localSpecialProcurementType;
+    private String localPlanningStrategyGroup;
+    private String localConsumptionPeriodBackward;
+    private String localConsumptionPeriodForward;
+    private String localConsumptionMode;
+    private String localGoodsReceiptProcessingTimeInDays;
+    private String localBatchManagementRequirmentIndicator;
+    private String localPlanningTimeFence;
+    private String localPosttoinspstk;
+    private String localComponentScrap;
+
+    public String getLocalDeletionFlagPlant() {
+        return localDeletionFlagPlant;
+    }
+
+    public String getLocalFixedlotsize() {
+        return localFixedlotsize;
+    }
+
+    public void setLocalFixedlotsize(String localFixedlotsize) {
+        this.localFixedlotsize = localFixedlotsize;
+    }
+
+    public String getLocalMaximumLotSize() {
+        return localMaximumLotSize;
+    }
+
+    public void setLocalMaximumLotSize(String localMaximumLotSize) {
+        this.localMaximumLotSize = localMaximumLotSize;
+    }
+
+    public String getLocalMinimumLotSize() {
+        return localMinimumLotSize;
+    }
+
+    public void setLocalMinimumLotSize(String localMinimumLotSize) {
+        this.localMinimumLotSize = localMinimumLotSize;
+    }
+
+    public String getLocalRoundingvalueForPoq() {
+        return localRoundingvalueForPoq;
+    }
+
+    public void setLocalRoundingvalueForPoq(String localRoundingvalueForPoq) {
+        this.localRoundingvalueForPoq = localRoundingvalueForPoq;
+    }
+
+    public String getLocalLotsize() {
+        return localLotsize;
+    }
+
+    public void setLocalLotsize(String localLotsize) {
+        this.localLotsize = localLotsize;
+    }
+
+    public String getLocalMrpType() {
+        return localMrpType;
+    }
+
+    public void setLocalMrpType(String localMrpType) {
+        this.localMrpType = localMrpType;
+    }
+
+    public String getLocalMrpController() {
+        return localMrpController;
+    }
+
+    public void setLocalMrpController(String localMrpController) {
+        this.localMrpController = localMrpController;
+    }
+
+    public String getLocalInHouseProcessingTime() {
+        return localInHouseProcessingTime;
+    }
+
+    public void setLocalInHouseProcessingTime(String localInHouseProcessingTime) {
+        this.localInHouseProcessingTime = localInHouseProcessingTime;
+    }
+
+    public String getLocalSafetyStock() {
+        return localSafetyStock;
+    }
+
+    public void setLocalSafetyStock(String localSafetyStock) {
+        this.localSafetyStock = localSafetyStock;
+    }
+
+    public String getLocalMinimumSafetyStock() {
+        return localMinimumSafetyStock;
+    }
+
+    public void setLocalMinimumSafetyStock(String localMinimumSafetyStock) {
+        this.localMinimumSafetyStock = localMinimumSafetyStock;
+    }
+
+    public String getLocalProductionSupervisor() {
+        return localProductionSupervisor;
+    }
+
+    public void setLocalProductionSupervisor(String localProductionSupervisor) {
+        this.localProductionSupervisor = localProductionSupervisor;
+    }
+
+    public String getLocalProductionUnit() {
+        return localProductionUnit;
+    }
+
+    public void setLocalProductionUnit(String localProductionUnit) {
+        this.localProductionUnit = localProductionUnit;
+    }
+
+    public String getLocalPosttoInspectionStock() {
+        return localPosttoInspectionStock;
+    }
+
+    public void setLocalPosttoInspectionStock(String localPosttoInspectionStock) {
+        this.localPosttoInspectionStock = localPosttoInspectionStock;
+    }
+
+    public String getLocalComponentScrapInPercent() {
+        return localComponentScrapInPercent;
+    }
+
+    public void setLocalComponentScrapInPercent(String localComponentScrapInPercent) {
+        this.localComponentScrapInPercent = localComponentScrapInPercent;
+    }
+
+    public String getLocalCriticalpart() {
+        return localCriticalpart;
+    }
+
+    public void setLocalCriticalpart(String localCriticalpart) {
+        this.localCriticalpart = localCriticalpart;
+    }
+
+    public String getLocalAbcIndicator() {
+        return localAbcIndicator;
+    }
+
+    public void setLocalAbcIndicator(String localAbcIndicator) {
+        this.localAbcIndicator = localAbcIndicator;
+    }
+
+    public String getLocalMaximumStocklevel() {
+        return localMaximumStocklevel;
+    }
+
+    public void setLocalMaximumStocklevel(String localMaximumStocklevel) {
+        this.localMaximumStocklevel = localMaximumStocklevel;
+    }
+
+    public String getLocalCheckingGroupforAvailabilityCheck() {
+        return localCheckingGroupforAvailabilityCheck;
+    }
+
+    public void setLocalCheckingGroupforAvailabilityCheck(String localCheckingGroupforAvailabilityCheck) {
+        this.localCheckingGroupforAvailabilityCheck = localCheckingGroupforAvailabilityCheck;
+    }
+
+    public String getLocalPlannedDeliveryTimeInDays() {
+        return localPlannedDeliveryTimeInDays;
+    }
+
+    public void setLocalPlannedDeliveryTimeInDays(String localPlannedDeliveryTimeInDays) {
+        this.localPlannedDeliveryTimeInDays = localPlannedDeliveryTimeInDays;
+    }
+
+    public String getLocalDependentRequirements() {
+        return localDependentRequirements;
+    }
+
+    public void setLocalDependentRequirements(String localDependentRequirements) {
+        this.localDependentRequirements = localDependentRequirements;
+    }
+
+    public String getLocalSafetytimeIndicator() {
+        return localSafetytimeIndicator;
+    }
+
+    public void setLocalSafetytimeIndicator(String localSafetytimeIndicator) {
+        this.localSafetytimeIndicator = localSafetytimeIndicator;
+    }
+
+    public String getLocalSafetyTime() {
+        return localSafetyTime;
+    }
+
+    public void setLocalSafetyTime(String localSafetyTime) {
+        this.localSafetyTime = localSafetyTime;
+    }
+
+    public String getLocalSpecialProcurementType() {
+        return localSpecialProcurementType;
+    }
+
+    public void setLocalSpecialProcurementType(String localSpecialProcurementType) {
+        this.localSpecialProcurementType = localSpecialProcurementType;
+    }
+
+    public String getLocalPlanningStrategyGroup() {
+        return localPlanningStrategyGroup;
+    }
+
+    public void setLocalPlanningStrategyGroup(String localPlanningStrategyGroup) {
+        this.localPlanningStrategyGroup = localPlanningStrategyGroup;
+    }
+
+    public String getLocalConsumptionPeriodBackward() {
+        return localConsumptionPeriodBackward;
+    }
+
+    public void setLocalConsumptionPeriodBackward(String localConsumptionPeriodBackward) {
+        this.localConsumptionPeriodBackward = localConsumptionPeriodBackward;
+    }
+
+    public String getLocalConsumptionPeriodForward() {
+        return localConsumptionPeriodForward;
+    }
+
+    public void setLocalConsumptionPeriodForward(String localConsumptionPeriodForward) {
+        this.localConsumptionPeriodForward = localConsumptionPeriodForward;
+    }
+
+    public String getLocalConsumptionMode() {
+        return localConsumptionMode;
+    }
+
+    public void setLocalConsumptionMode(String localConsumptionMode) {
+        this.localConsumptionMode = localConsumptionMode;
+    }
+
+    public String getLocalGoodsReceiptProcessingTimeInDays() {
+        return localGoodsReceiptProcessingTimeInDays;
+    }
+
+    public void setLocalGoodsReceiptProcessingTimeInDays(String localGoodsReceiptProcessingTimeInDays) {
+        this.localGoodsReceiptProcessingTimeInDays = localGoodsReceiptProcessingTimeInDays;
+    }
+
+    public String getLocalBatchManagementRequirmentIndicator() {
+        return localBatchManagementRequirmentIndicator;
+    }
+
+    public void setLocalBatchManagementRequirmentIndicator(String localBatchManagementRequirmentIndicator) {
+        this.localBatchManagementRequirmentIndicator = localBatchManagementRequirmentIndicator;
+    }
+
+    public String getLocalPlanningTimeFence() {
+        return localPlanningTimeFence;
+    }
+
+    public void setLocalPlanningTimeFence(String localPlanningTimeFence) {
+        this.localPlanningTimeFence = localPlanningTimeFence;
+    }
+
+    public String getLocalPosttoinspstk() {
+        return localPosttoinspstk;
+    }
+
+    public void setLocalPosttoinspstk(String localPosttoinspstk) {
+        this.localPosttoinspstk = localPosttoinspstk;
+    }
+
+    public String getLocalComponentScrap() {
+        return localComponentScrap;
+    }
+
+    public void setLocalComponentScrap(String localComponentScrap) {
+        this.localComponentScrap = localComponentScrap;
+    }
+
+    public void setLocalDeletionFlagPlant(String localDeletionFlagPlant) {
+        this.localDeletionFlagPlant = localDeletionFlagPlant;
+    }
 
     @Override
     public String getKey() {
@@ -77,4 +375,8 @@ public class EDMMaterialPlantBo  extends BaseBo
     public void setPlantStatus(String plantStatus) {
         this.plantStatus = plantStatus;
     }
+
+    public String getLocalProcurementType() { return localProcurementType; }
+
+    public void setLocalProcurementType(String localProcurementType) { this.localProcurementType = localProcurementType; }
 }
--- pangea-parent/pangea-view/src/main/java/com/jnj/pangea/edm/material/plant/service/EDMMaterialPlantServiceImpl.java
+++ pangea-parent/pangea-view/src/main/java/com/jnj/pangea/edm/material/plant/service/EDMMaterialPlantServiceImpl.java
@@ -2,6 +2,7 @@ package com.jnj.pangea.edm.material.plant.service;
 
 import com.jnj.adf.client.api.query.QueryHelper;
 import com.jnj.adf.curation.indexer.AdfLuceneHelper;
+import com.jnj.adf.grid.utils.LogUtil;
 import com.jnj.pangea.common.IConstant;
 import com.jnj.pangea.common.ResultObject;
 import com.jnj.pangea.common.dao.impl.EDMMatPlantStatV1DaoImpl;
@@ -43,16 +44,14 @@ public class EDMMaterialPlantServiceImpl implements ICommonService {
 
         EDMMaterialPlantBo materialPlantBo = new EDMMaterialPlantBo();
 
-        EDMSourceSystemV1Entity sourceSystemV1Entity = sourceSystemV1Dao.getSourceSystemWithProjectOne();
-        String sourceSystem = null;
-        if (null != sourceSystemV1Entity){
-            sourceSystem = sourceSystemV1Entity.getSourceSystem();
-            materialPlantBo.setSourceSystem(sourceSystem);
-        }
+        String sourceSystem = sourceSystemV1Dao.getSourceSystemWithProjectOne().getSourceSystem();
+        materialPlantBo.setSourceSystem(sourceSystem);
+
         String matnr = marcEntity.getMatnr();
         materialPlantBo.setLocalMaterialNumber(matnr);
         String werks = marcEntity.getWerks();
         materialPlantBo.setLocalPlant(werks);
+        materialPlantBo.setLocalDeletionFlagPlant(werks);
 
         EDMMaterialGlobalV1Entity materialGlobalV1Entity = materialGlobalDao.getMaterialNumberWithLocalMaterialNumberAndSourceSystem(sourceSystem,matnr);
         if (null != materialGlobalV1Entity){
@@ -74,6 +73,104 @@ public class EDMMaterialPlantServiceImpl implements ICommonService {
             String plantStatus = matPlantStatV1Entity.getPlantStatus();
             materialPlantBo.setPlantStatus(plantStatus);
         }
+
+        String beskz = marcEntity.getBeskz();
+        materialPlantBo.setLocalProcurementType(beskz);
+
+        String bstfe = marcEntity.getBstfe();
+        materialPlantBo.setLocalFixedlotsize(bstfe);
+
+        String bstma = marcEntity.getBstma();
+        materialPlantBo.setLocalMaximumLotSize(bstma);
+
+        String bstmi = marcEntity.getBstmi();
+        materialPlantBo.setLocalMinimumLotSize(bstmi);
+
+        String bstrf = marcEntity.getBstrf();
+        materialPlantBo.setLocalRoundingvalueForPoq(bstrf);
+
+        String disls = marcEntity.getDisls();
+        materialPlantBo.setLocalLotsize(disls);
+
+        String dismm = marcEntity.getDismm();
+        materialPlantBo.setLocalMrpType(dismm);
+
+        String dispo = marcEntity.getDispo();
+        materialPlantBo.setLocalMrpController(dispo);
+
+        String dzeit = marcEntity.getDzeit();
+        materialPlantBo.setLocalInHouseProcessingTime(dzeit);
+
+        String eisbe = marcEntity.getEisbe();
+        materialPlantBo.setLocalSafetyStock(eisbe);
+
+        String eislo = marcEntity.getEislo();
+        materialPlantBo.setLocalMinimumSafetyStock(eislo);
+
+        String fevor = marcEntity.getFevor();
+        materialPlantBo.setLocalProductionSupervisor(fevor);
+
+        String frtme = marcEntity.getFrtme();
+        materialPlantBo.setLocalProductionUnit(frtme);
+
+        String insmk = marcEntity.getInsmk();
+        materialPlantBo.setLocalPosttoInspectionStock(insmk);
+
+
+        String kausf = marcEntity.getKausf();
+        materialPlantBo.setLocalComponentScrapInPercent(kausf);
+
+        String kzkri = marcEntity.getKzkri();
+        materialPlantBo.setLocalCriticalpart(kzkri);
+
+        String maabc = marcEntity.getMaabc();
+        materialPlantBo.setLocalAbcIndicator(maabc);
+
+        String mabst = marcEntity.getMabst();
+        materialPlantBo.setLocalMaximumStocklevel(mabst);
+
+        String mtvfp = marcEntity.getMtvfp();
+        materialPlantBo.setLocalCheckingGroupforAvailabilityCheck(mtvfp);
+
+        String plifz = marcEntity.getPlifz();
+        materialPlantBo.setLocalPlannedDeliveryTimeInDays(plifz);
+
+        String sbdkz = marcEntity.getSbdkz();
+        materialPlantBo.setLocalDependentRequirements(sbdkz);
+
+        String shflg = marcEntity.getShflg();
+        materialPlantBo.setLocalSafetytimeIndicator(shflg);
+
+        String shzet = marcEntity.getShzet();
+        materialPlantBo.setLocalSafetyTime(shzet);
+
+        String sobsl = marcEntity.getSobsl();
+        materialPlantBo.setLocalSpecialProcurementType(sobsl);
+
+        String strgr = marcEntity.getStrgr();
+        materialPlantBo.setLocalPlanningStrategyGroup(strgr);
+
+        String vint1 = marcEntity.getVint1();
+        materialPlantBo.setLocalConsumptionPeriodBackward(vint1);
+
+        String vint2 = marcEntity.getVint2();
+        materialPlantBo.setLocalConsumptionPeriodForward(vint2);
+
+        String vrmod = marcEntity.getVrmod();
+        materialPlantBo.setLocalConsumptionMode(vrmod);
+
+        String webaz = marcEntity.getWebaz();
+        materialPlantBo.setLocalGoodsReceiptProcessingTimeInDays(webaz);
+
+        String xchpf = marcEntity.getXchpf();
+        materialPlantBo.setLocalBatchManagementRequirmentIndicator(xchpf);
+
+        String fxhor = marcEntity.getFxhor();
+        materialPlantBo.setLocalPlanningTimeFence(fxhor);
+
+        materialPlantBo.setLocalPosttoinspstk(insmk);
+
+        materialPlantBo.setLocalComponentScrap(kausf);
 
         resultObject.setBaseBo(materialPlantBo);
 
