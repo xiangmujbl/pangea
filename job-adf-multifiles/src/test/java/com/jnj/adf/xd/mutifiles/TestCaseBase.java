02b0e107b8697c6587b0fc8c7a2c69f264ad2d29
KDing5 <kding5@its.jnj.com>
2018-03-23 13:57:58 +0800

    modify script

---
+++ job-adf-multifiles/src/test/java/com/jnj/adf/xd/mutifiles/TestCaseBase.java
@@ -0,0 +1,30 @@
+package com.jnj.adf.xd.mutifiles;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:testJobConfig/context-config.xml", "classpath:config/adfMutifiles.xml"})
+public class TestCaseBase {
+
+    @Autowired
+    protected JobLauncherTestUtils launcher;
+
+    @Test
+    public void launchJob() {
+        try {
+            JobExecution execution = launcher.launchJob();
+            Assert.assertEquals(BatchStatus.COMPLETED, execution.getStatus());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
