3607380b9c8b9840c290631b43858f7cc68d3bb2
KDing5 <kding5@its.jnj.com>
2018-03-26 10:42:20 +0800

    add tools

--- pangea-tools/src/main/java/com/jnj/pangea/GenerateTool.java
+++ pangea-tools/src/main/java/com/jnj/pangea/GenerateTool.java
@@ -5,9 +5,14 @@ public class GenerateTool {
     public static void main(String[] args) {
 
         ViewCreator viewCreator = new ViewCreator("system", "test_view", "AEAZ-123", true);
+        // if the name contains an abbreviation, use following
+//        ViewCreator viewCreator = new ViewCreator("system", "TestView", "test_view", "SystemTestView", "AEAZ-123", true);
+
+        // first step
         viewCreator.generateTestJavaFile();
         viewCreator.generateTestFeatureFile();
         viewCreator.generateCurationXMLFile();
+        // fill the xml, and run below
         viewCreator.generateCurationCodeFile();
     }
 }
