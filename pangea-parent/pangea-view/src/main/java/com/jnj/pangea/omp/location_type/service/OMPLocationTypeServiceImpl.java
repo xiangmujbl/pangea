fffbc19cc6c8e7082de381ba73ddc67aa8bb16fc
KDing5 <kding5@its.jnj.com>
2018-03-27 15:14:25 +0800

    add /omp/location_type

---
+++ pangea-parent/pangea-view/src/main/java/com/jnj/pangea/omp/location_type/service/OMPLocationTypeServiceImpl.java
@@ -0,0 +1,38 @@
+package com.jnj.pangea.omp.location_type.service;
+
+import com.jnj.pangea.common.IConstant;
+import com.jnj.pangea.common.ResultObject;
+import com.jnj.pangea.common.entity.plan.PlanCnsLocationTypeEntity;
+import com.jnj.pangea.common.service.ICommonService;
+import com.jnj.pangea.omp.location_type.bo.OMPLocationTypeBo;
+
+public class OMPLocationTypeServiceImpl implements ICommonService {
+
+    private static OMPLocationTypeServiceImpl instance;
+
+    public static OMPLocationTypeServiceImpl getInstance() {
+        if (instance == null) {
+            instance = new OMPLocationTypeServiceImpl();
+        }
+        return instance;
+    }
+
+
+    @Override
+    public ResultObject buildView(String key, Object o, Object o2) {
+
+        ResultObject resultObject = new ResultObject();
+        PlanCnsLocationTypeEntity cnsLocationTypeEntity = (PlanCnsLocationTypeEntity) o;
+
+        OMPLocationTypeBo locationTypeBo = new OMPLocationTypeBo();
+
+        locationTypeBo.setLocationTypeId(cnsLocationTypeEntity.getPlanLocTypeId());
+        locationTypeBo.setLabel(cnsLocationTypeEntity.getPlanLocTypeDesc());
+        locationTypeBo.setActiveFctErp(IConstant.VALUE.YES);
+        locationTypeBo.setActiveOprErp(IConstant.VALUE.YES);
+        locationTypeBo.setActiveSopErp(IConstant.VALUE.YES);
+
+        resultObject.setBaseBo(locationTypeBo);
+        return resultObject;
+    }
+}
