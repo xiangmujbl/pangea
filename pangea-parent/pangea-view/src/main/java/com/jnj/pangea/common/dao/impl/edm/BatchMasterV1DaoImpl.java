3d4413fd704d44e9fee41923ea4c23b4349192c0
JGuo57 <jguo57@its.jnj.com>
2018-03-27 11:49:59 +0800

    batch master v1 changes

---
+++ pangea-parent/pangea-view/src/main/java/com/jnj/pangea/common/dao/impl/edm/BatchMasterV1DaoImpl.java
@@ -0,0 +1,27 @@
+package com.jnj.pangea.common.dao.impl.edm;
+
+import com.jnj.pangea.common.dao.impl.CommonDaoImpl;
+import com.jnj.adf.client.api.query.QueryHelper;
+import com.jnj.pangea.common.IConstant;
+import com.jnj.pangea.common.entity.edm.BatchMasterV1Entity;
+
+public class BatchMasterV1DaoImpl extends CommonDaoImpl {
+
+    private static BatchMasterV1DaoImpl instance;
+
+    public static BatchMasterV1DaoImpl getInstance() {
+        if (instance == null) {
+            instance = new BatchMasterV1DaoImpl();
+        }
+        return instance;
+    }
+
+    public BatchMasterV1Entity getEntityWithConditions(String param) {
+
+        //String queryString = QueryHelper.buildCriteria(IConstant.EDM_COUNTRY_V1.LOCAL_COUNTRY).is(land1).toQueryString();
+        //return queryForObject(IConstant.REGION.EDM_COUNTRY_V1, localQueryString, EDMCountryEntity.class);
+        return null;
+    }
+
+
+}
---
+++ pangea-parent/pangea-view/src/main/java/com/jnj/pangea/edm/batch_master/service/EDMBatchMasterServiceImpl.java
@@ -0,0 +1,60 @@
+package com.jnj.pangea.edm.batch_master.service;
+
+import com.jnj.adf.grid.utils.LogUtil;
+import com.jnj.pangea.common.ResultObject;
+import com.jnj.pangea.common.dao.impl.edm.EDMMaterialGlobalDaoImpl;
+import com.jnj.pangea.common.dao.impl.edm.EDMSourceSystemV1DaoImpl;
+import com.jnj.pangea.common.entity.edm.EDMMaterialGlobalV1Entity;
+import com.jnj.pangea.common.entity.edm.EDMSourceSystemV1Entity;
+import com.jnj.pangea.common.entity.project_one.Mch1Entity;
+import com.jnj.pangea.common.service.ICommonService;
+import com.jnj.pangea.edm.batch_master.bo.EDMBatchMasterBo;
+
+public class EDMBatchMasterServiceImpl implements ICommonService {
+
+    private static EDMBatchMasterServiceImpl instance;
+
+    public static EDMBatchMasterServiceImpl getInstance() {
+        if (instance == null) {
+            instance = new EDMBatchMasterServiceImpl();
+        }
+        return instance;
+    }
+
+    private EDMSourceSystemV1DaoImpl sourceSystemV1Dao = EDMSourceSystemV1DaoImpl.getInstance();
+    private EDMMaterialGlobalDaoImpl materialGlobalV1Dao = EDMMaterialGlobalDaoImpl.getInstance();
+
+    @Override
+    public ResultObject buildView(String key, Object o, Object o2) {
+
+        ResultObject resultObject = new ResultObject();
+        Mch1Entity mch1Entity = (Mch1Entity) o;
+        LogUtil.getCoreLog().info("mch1Entity:{}",mch1Entity.toString());
+        EDMBatchMasterBo eDMBatchMasterBo = new EDMBatchMasterBo();
+
+        // TODO add logic
+        EDMSourceSystemV1Entity edmSourceSystemV1Entity = sourceSystemV1Dao.getSourceSystemWithProjectOne();
+        eDMBatchMasterBo.setSourceSystem(edmSourceSystemV1Entity.getSourceSystem());
+        processSystem(mch1Entity,eDMBatchMasterBo);
+        LogUtil.getCoreLog().info("getMatnr:{},getSourceSystem:{}",mch1Entity.getMatnr(),edmSourceSystemV1Entity.getSourceSystem());
+        EDMMaterialGlobalV1Entity materialGlobalV1Entity =  materialGlobalV1Dao.getMaterialNumberWithLocalMaterialNumberAndSourceSystem(mch1Entity.getMatnr(),edmSourceSystemV1Entity.getSourceSystem());
+
+        if(materialGlobalV1Entity!=null){
+            LogUtil.getCoreLog().info("materialGlobalV1Entity:{}",materialGlobalV1Entity.toString());
+            eDMBatchMasterBo.setMaterialNumber(materialGlobalV1Entity.getMaterialNumber());
+        }else{
+            eDMBatchMasterBo.setMaterialNumber("");
+        }
+        LogUtil.getCoreLog().info("eDMBatchMasterBo:{}",eDMBatchMasterBo.toString());
+        resultObject.setBaseBo(eDMBatchMasterBo);
+        return resultObject;
+    }
+
+
+    private void processSystem(Mch1Entity mainData, EDMBatchMasterBo edmBatchMasterBo) {
+        edmBatchMasterBo.setLocalBatchNumber(mainData.getCharg());
+        edmBatchMasterBo.setLocalDateofManufacture(mainData.getHsdat());
+        edmBatchMasterBo.setLocalMaterialNumber(mainData.getMatnr());
+        edmBatchMasterBo.setLocalShelfLifeExpiration(mainData.getVfdat());
+    }
+}
