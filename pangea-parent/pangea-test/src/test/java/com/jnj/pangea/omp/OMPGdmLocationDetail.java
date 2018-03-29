213c6e8f027a5c4cdee2793b02cbc4625f7dcf1a
XFu22 <XFu22@its.jnj.com>
2018-03-29 16:19:34 +0800

    Add OMPGdmLocationDetail

---
+++ pangea-parent/pangea-test/src/test/java/com/jnj/pangea/omp/OMPGdmLocationDetail.java
@@ -0,0 +1,14 @@
+package com.jnj.pangea.omp;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/test/resources/features/omp/OMPGdmLocationDetail.feature"},
+        tags = {"@pangea_test"},
+        glue = "com.jnj.pangea.sentence",
+        plugin = {"pretty", "json:target/reports/json/ OMPGdmLocationDetail.json"})
+public class OMPGdmLocationDetail {
+}
