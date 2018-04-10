fbf62854db90c24f5eef6a50f9c77d2ab8b1aea4
KDing5 <kding5@its.jnj.com>
2018-04-10 21:06:01 +0800

    AEAZ-1431 fix bugs

--- pangea-parent/pangea-view/src/main/java/com/jnj/pangea/edm/mat_plant_fi/service/EDMMatPlantFiServiceImpl.java
+++ pangea-parent/pangea-view/src/main/java/com/jnj/pangea/edm/mat_plant_fi/service/EDMMatPlantFiServiceImpl.java
@@ -1,11 +1,15 @@
 package com.jnj.pangea.edm.mat_plant_fi.service;
 
+import com.jnj.pangea.common.IConstant;
 import com.jnj.pangea.common.ResultObject;
 import com.jnj.pangea.common.dao.impl.edm.EDMPlantV1DaoImpl;
 import com.jnj.pangea.common.dao.impl.edm.EDMSourceSystemV1DaoImpl;
+import com.jnj.pangea.common.entity.edm.EDMPlantV1Entity;
+import com.jnj.pangea.common.entity.edm.EDMSourceSystemV1Entity;
 import com.jnj.pangea.common.entity.project_one.MbewEntity;
 import com.jnj.pangea.common.service.ICommonService;
 import com.jnj.pangea.edm.mat_plant_fi.bo.EDMMatPlantFiBo;
+import org.apache.commons.lang3.StringUtils;
 
 public class EDMMatPlantFiServiceImpl implements ICommonService {
 
@@ -29,8 +33,13 @@ public class EDMMatPlantFiServiceImpl implements ICommonService {
         MbewEntity mbewEntity = (MbewEntity) o;
 
         EDMMatPlantFiBo matPlantFiBo = new EDMMatPlantFiBo();
-        String sourceSystem = sourceSystemV1Dao.getSourceSystemWithProjectOne().getSourceSystem();
-        matPlantFiBo.setSourceSystem(sourceSystem);
+
+        String sourceSystem = "";
+        EDMSourceSystemV1Entity sourceSystemV1Entity = sourceSystemV1Dao.getEntityWithLocalSourceSystem(IConstant.VALUE.PROJECT_ONE);
+        if (null != sourceSystemV1Entity) {
+            sourceSystem = sourceSystemV1Entity.getSourceSystem();
+            matPlantFiBo.setSourceSystem(sourceSystem);
+        }
 
         matPlantFiBo.setLocalMaterialNumber(mbewEntity.getMatnr());
 
@@ -38,8 +47,12 @@ public class EDMMatPlantFiServiceImpl implements ICommonService {
         matPlantFiBo.setLocalPlant(bwkey);
         matPlantFiBo.setLocalDeleIndicator(mbewEntity.getLvorm());
 
-        String plant = plantV1Dao.getPlantWithSourceSystemAndLocalPlant(sourceSystem, bwkey).getPlant();
-        matPlantFiBo.setPlant(plant);
+        if (StringUtils.isNotEmpty(sourceSystem) && StringUtils.isNotEmpty(bwkey)) {
+            EDMPlantV1Entity plantV1Entity = plantV1Dao.getPlantWithSourceSystemAndLocalPlant(sourceSystem, bwkey);
+            if (null != plantV1Entity) {
+                matPlantFiBo.setPlant(plantV1Entity.getPlant());
+            }
+        }
 
         matPlantFiBo.setPriceControl(mbewEntity.getVprsv());
         matPlantFiBo.setLocalStandardPrice(mbewEntity.getStprs());
