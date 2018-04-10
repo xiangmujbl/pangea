396f4677c87475a7303d2610a055d71f74d1ef4c
jhuan127 <Welcome8>
2018-04-10 10:15:54 +0800

    ADD OMPCertainity AEAZ-2367

--- pangea-parent/pangea-test/src/test/java/com/jnj/pangea/omp/OMPCertainityTest.java
+++ pangea-parent/pangea-test/src/test/java/com/jnj/pangea/omp/OMPCertainityTest.java
@@ -9,6 +9,6 @@ import org.junit.runner.RunWith;
         features = {"src/test/resources/features/omp/OMPCertainity.feature"},
         tags = {"@pangea_test"},
         glue = "com.jnj.pangea.sentence",
-        plugin = {"pretty", "json:target/reports/json/ OMPCertainity.json"})
+        plugin = {"pretty", "json:target/reports/json/AEAZ-2367 OMPCertainity.json"})
 public class OMPCertainityTest {
 }
