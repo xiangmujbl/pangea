770ca3d7b299bfb6c62d3144b667e666c585e8f0
FShen17 <fshen17@its.jnj.com>
2018-04-10 14:12:20 -0400

    AEAZ-1980 update GDMUnit_Measurable

--- pangea-parent/pangea-view/src/main/java/com/jnj/pangea/omp/gdm_unit_measurable/service/OMPGdmUnitMeasurableServiceImpl.java
+++ pangea-parent/pangea-view/src/main/java/com/jnj/pangea/omp/gdm_unit_measurable/service/OMPGdmUnitMeasurableServiceImpl.java
@@ -8,6 +8,7 @@ import com.jnj.pangea.common.entity.edm.EDMUnitOfMeasureV1Entity;
 import com.jnj.pangea.common.entity.plan.CnsPlanUnitEntity;
 import com.jnj.pangea.common.service.ICommonService;
 import com.jnj.pangea.omp.gdm_unit_measurable.bo.OMPGdmUnitMeasurableBo;
+import org.apache.commons.lang.StringUtils;
 
 public class OMPGdmUnitMeasurableServiceImpl implements ICommonService {
 
@@ -30,25 +31,27 @@ public class OMPGdmUnitMeasurableServiceImpl implements ICommonService {
 
         OMPGdmUnitMeasurableBo gdmUnitMeasurableBo = new OMPGdmUnitMeasurableBo();
         String localUom = unitOfMeasureV1Entity.getLocalUom();
-        CnsPlanUnitEntity cnsPlanUnitEntity = cnsPlanUnitDao.getCnsPlanUnitEntityWithLocalUom(localUom);
-        if (null != cnsPlanUnitEntity) {
-            gdmUnitMeasurableBo.setUnitId(cnsPlanUnitEntity.getUnit());
-            gdmUnitMeasurableBo.setActive(IConstant.VALUE.YES);
-            gdmUnitMeasurableBo.setActiveFCTERP(IConstant.VALUE.YES);
-            gdmUnitMeasurableBo.setActiveOPRERP(IConstant.VALUE.YES);
-            gdmUnitMeasurableBo.setActiveSOPERP(IConstant.VALUE.YES);
-            gdmUnitMeasurableBo.setFactor(unitOfMeasureV1Entity.getFactor());
-            gdmUnitMeasurableBo.setIsoCode(unitOfMeasureV1Entity.getIsoCode());
-            gdmUnitMeasurableBo.setMeasure(unitOfMeasureV1Entity.getMeasure());
-            gdmUnitMeasurableBo.setPrecision(unitOfMeasureV1Entity.getRoundingDecimal());
-            gdmUnitMeasurableBo.setLongDescription(unitOfMeasureV1Entity.getUomName());
-            gdmUnitMeasurableBo.setShortDescription(unitOfMeasureV1Entity.getUomName());
-            resultObject.setBaseBo(gdmUnitMeasurableBo);
+        if (StringUtils.isNotEmpty(localUom)) {
+            CnsPlanUnitEntity cnsPlanUnitEntity = cnsPlanUnitDao.getCnsPlanUnitEntityWithLocalUom(localUom);
+            if (null != cnsPlanUnitEntity) {
+                gdmUnitMeasurableBo.setUnitId(cnsPlanUnitEntity.getUnit());
+                gdmUnitMeasurableBo.setActive(IConstant.VALUE.YES);
+                gdmUnitMeasurableBo.setActiveFCTERP(IConstant.VALUE.YES);
+                gdmUnitMeasurableBo.setActiveOPRERP(IConstant.VALUE.YES);
+                gdmUnitMeasurableBo.setActiveSOPERP(IConstant.VALUE.YES);
+                gdmUnitMeasurableBo.setFactor(unitOfMeasureV1Entity.getFactor());
+                gdmUnitMeasurableBo.setIsoCode(unitOfMeasureV1Entity.getIsoCode());
+                gdmUnitMeasurableBo.setMeasure(unitOfMeasureV1Entity.getMeasure());
+                gdmUnitMeasurableBo.setPrecision(unitOfMeasureV1Entity.getRoundingDecimal());
+                gdmUnitMeasurableBo.setLongDescription(unitOfMeasureV1Entity.getUomName());
+                gdmUnitMeasurableBo.setShortDescription(unitOfMeasureV1Entity.getUomName());
+                resultObject.setBaseBo(gdmUnitMeasurableBo);
 
-        } else {
-            resultObject.setFailData(new FailData(IConstant.FAILED.FUNCTIONAL_AREA.SP, IConstant.FAILED.INTERFACE_ID.GDM_UNIT_MEASURABLE, IConstant.FAILED.ERROR_CODE.T1,
-                    "Enterprise UOM is missing for local UOM", "", unitOfMeasureV1Entity.getLocalUom(),
-                    unitOfMeasureV1Entity.getSourceSystem()));
+            } else {
+                resultObject.setFailData(new FailData(IConstant.FAILED.FUNCTIONAL_AREA.SP, IConstant.FAILED.INTERFACE_ID.GDM_UNIT_MEASURABLE, IConstant.FAILED.ERROR_CODE.T1,
+                        "Enterprise UOM is missing for local UOM", "", unitOfMeasureV1Entity.getLocalUom(),
+                        unitOfMeasureV1Entity.getSourceSystem()));
+            }
         }
         return resultObject;
     }
