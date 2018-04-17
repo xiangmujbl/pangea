9d9259a0b8d1e34f3a1a8de294ec1334e8308f95
FShen17 <fshen17@its.jnj.com>
2018-04-17 05:21:29 -0400

    AEAZ-?? add DOMD_GDMRegion

---
+++ pangea-parent/pangea-view/src/main/java/com/jnj/pangea/omp/gdm_region/service/OMPGdmRegionServiceImpl.java
@@ -0,0 +1,39 @@
+package com.jnj.pangea.omp.gdm_region.service;
+
+import com.jnj.pangea.common.IConstant;
+import com.jnj.pangea.common.ResultObject;
+import com.jnj.pangea.common.entity.plan.PlanCnsPlanRegionEntity;
+import com.jnj.pangea.common.service.ICommonService;
+import com.jnj.pangea.omp.gdm_region.bo.OMPGdmRegionBo;
+
+public class OMPGdmRegionServiceImpl implements ICommonService {
+
+    private static OMPGdmRegionServiceImpl instance;
+
+    public static OMPGdmRegionServiceImpl getInstance() {
+        if (instance == null) {
+            instance = new OMPGdmRegionServiceImpl();
+        }
+        return instance;
+    }
+
+
+    @Override
+    public ResultObject buildView(String key, Object o, Object o2) {
+
+        ResultObject resultObject = new ResultObject();
+        PlanCnsPlanRegionEntity cnsPlanRegionEntity = (PlanCnsPlanRegionEntity) o;
+
+        OMPGdmRegionBo gdmRegionBo = new OMPGdmRegionBo();
+
+        gdmRegionBo.setRegionId(cnsPlanRegionEntity.getPlanningRegionId());
+        gdmRegionBo.setRegionDescription(cnsPlanRegionEntity.getPlanningRegionDesc());
+
+        gdmRegionBo.setActiveFCTERP(IConstant.VALUE.YES);
+        gdmRegionBo.setActiveOPRERP(IConstant.VALUE.YES);
+        gdmRegionBo.setActiveSOPERP(IConstant.VALUE.YES);
+
+        resultObject.setBaseBo(gdmRegionBo);
+        return resultObject;
+    }
+}
