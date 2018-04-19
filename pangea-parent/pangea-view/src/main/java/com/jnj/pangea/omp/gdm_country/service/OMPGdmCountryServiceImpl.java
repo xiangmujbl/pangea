9224c4898719a05dfa3debd9104ff9ccc76ca1de
KDing5 <kding5@its.jnj.com>
2018-04-19 15:11:57 +0800

    AEAZ-1614 fix bug

--- pangea-parent/pangea-view/src/main/java/com/jnj/pangea/omp/gdm_country/service/OMPGdmCountryServiceImpl.java
+++ pangea-parent/pangea-view/src/main/java/com/jnj/pangea/omp/gdm_country/service/OMPGdmCountryServiceImpl.java
@@ -2,9 +2,14 @@ package com.jnj.pangea.omp.gdm_country.service;
 
 import com.jnj.pangea.common.IConstant;
 import com.jnj.pangea.common.ResultObject;
+import com.jnj.pangea.common.dao.impl.plan.PlanCnsPlanParameterDaoImpl;
 import com.jnj.pangea.common.entity.edm.EDMCountryV1Entity;
+import com.jnj.pangea.common.entity.plan.PlanCnsPlanParameterEntity;
 import com.jnj.pangea.common.service.ICommonService;
 import com.jnj.pangea.omp.gdm_country.bo.OMPGdmCountryBo;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.List;
 
 public class OMPGdmCountryServiceImpl implements ICommonService {
 
@@ -17,6 +22,8 @@ public class OMPGdmCountryServiceImpl implements ICommonService {
         return instance;
     }
 
+    private PlanCnsPlanParameterDaoImpl cnsPlanParameterDao = PlanCnsPlanParameterDaoImpl.getInstance();
+
     @Override
     public ResultObject buildView(String key, Object o, Object o2) {
 
@@ -24,10 +31,18 @@ public class OMPGdmCountryServiceImpl implements ICommonService {
         EDMCountryV1Entity countryV1Entity = (EDMCountryV1Entity) o;
 
         OMPGdmCountryBo gdmCountryBo = new OMPGdmCountryBo();
-        if (null != countryV1Entity) {
-            gdmCountryBo.setCountryId(countryV1Entity.getCountryCode());
-            gdmCountryBo.setCountryDescription(countryV1Entity.getCountryName());
+
+        // F1
+        String sourceSystem = countryV1Entity.getSourceSystem();
+        if (StringUtils.isNotEmpty(sourceSystem)) {
+            List<PlanCnsPlanParameterEntity> cnsPlanParameterEntities = cnsPlanParameterDao.getEntitiesWithSourceSystem(sourceSystem);
+            if (cnsPlanParameterEntities.isEmpty()) {
+                return resultObject;
+            }
         }
+
+        gdmCountryBo.setCountryId(countryV1Entity.getCountryCode());
+        gdmCountryBo.setCountryDescription(countryV1Entity.getCountryName());
         gdmCountryBo.setActiveFCTERP(IConstant.VALUE.YES);
         gdmCountryBo.setActiveOPRERP(IConstant.VALUE.YES);
         gdmCountryBo.setActiveSOPERP(IConstant.VALUE.NO);
