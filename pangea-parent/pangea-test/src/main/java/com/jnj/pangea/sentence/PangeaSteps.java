91c07a05071c9d963d417c46bd0ea5eef949199c
KDing5 <kding5@its.jnj.com>
2018-04-24 10:56:22 +0800

    AEAZ-1432 add consumption config

--- pangea-parent/pangea-test/src/main/java/com/jnj/pangea/sentence/PangeaSteps.java
+++ pangea-parent/pangea-test/src/main/java/com/jnj/pangea/sentence/PangeaSteps.java
@@ -4,24 +4,45 @@ import com.jnj.adf.cucumber.sentence.CommonSteps;
 import com.jnj.adf.curation.ComputeClient;
 import com.jnj.adf.grid.support.system.ADFConfigHelper;
 import com.jnj.adf.grid.utils.Util;
-import cucumber.api.Scenario;
-import cucumber.api.java.Before;
+import cucumber.api.DataTable;
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.errors.SerializationException;
 import org.junit.Assert;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
 
 public class PangeaSteps extends CommonSteps {
 
     private static String computingNode = "";
     private static Integer computingPartition = 1;
+    private static String mboxSink = "";
+    private static String bootstrapServers = "";
+    private static String schemaRegistryUrl = "";
+    private static String topic = "";
 
     static {
         computingNode = ADFConfigHelper.getProperty("computingNode");
         String partition = ADFConfigHelper.getProperty("computingPartition");
         if (StringUtils.isNotEmpty(partition))
             computingPartition = Integer.parseInt(partition);
+        mboxSink = ADFConfigHelper.getProperty("mboxSink");
+        bootstrapServers = ADFConfigHelper.getProperty("bootstrap.servers");
+        schemaRegistryUrl = ADFConfigHelper.getProperty("schema.registry.url");
+        topic = ADFConfigHelper.getProperty("kafka.topic");
     }
 
     public PangeaSteps() {
@@ -36,13 +57,13 @@ public class PangeaSteps extends CommonSteps {
             long count1 = 0;
             String[] region1 = compare1.split(",");
             for (String region : region1) {
-                count1 += adfService.onPath(region).count();
+                count1 += adfService.onPath(region).queryOql("select * from " + region).size();
             }
 
             long count2 = 0;
             String[] region2 = compare2.split(",");
             for (String region : region2) {
-                count2 += adfService.onPath(region).count();
+                count2 += adfService.onPath(region).queryOql("select * from " + region).size();
             }
             Assert.assertEquals(count1, count2);
         });
@@ -50,24 +71,99 @@ public class PangeaSteps extends CommonSteps {
         And("^I will remove all data with region \"([^\"]*)\"$", (String region) -> {
             adfService.onPath(region).removeAll();
         });
+
+
+        Then("^A file is found on sink application with name \"([^\"]*)\"$", (String fileName) -> {
+            retrieveFile(fileName);
+        });
+
+        And("^I check file data for filename \"([^\"]*)\" by keyFields \"([^\"]*)\"$", (String fileName, String keyFields, DataTable table) -> {
+//            File file = retrieveFile(fileName);
+            File file = new File("C:\\tmp\\"+fileName);
+            this.checkFileData(table.raw(), keyFields.split(","), file);
+        });
+
+        And("^I will remove the test file on sink application \"([^\"]*)\"$", (fileName) -> {
+            RestTemplate restTemplate = new RestTemplate();
+            restTemplate.delete("http://" + mboxSink + "/api/file/" + fileName);
+        });
+    }
+
+    private File retrieveFile(String fileName) {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getMessageConverters().add(new ByteArrayHttpMessageConverter());
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_OCTET_STREAM));
+        HttpEntity<String> entity = new HttpEntity<String>(headers);
+
+        ResponseEntity<byte[]> response = restTemplate.exchange("http://" + mboxSink + "/api/file/" + fileName, HttpMethod.GET, entity, byte[].class, "1");
+
+        File file = new File("tmp.tsv");
+        FileOutputStream output = null;
+        if(response.getStatusCode().equals(HttpStatus.OK))
+        {
+
+            try (FileOutputStream fileOutputStream = output = new FileOutputStream(file)) {
+                IOUtils.write(response.getBody(), output);
+            } catch (FileNotFoundException ex) {
+                ex.printStackTrace();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+
+
+        }
+
+        Assert.assertTrue(file.exists());
+        Assert.assertTrue(file.length() > 0);
+
+        return file;
+    }
+
+    private void checkFileData(List<List<String>> list, String[] keyFields, File file) {
+
+        try {
+            try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+                String line = bufferedReader.readLine();
+                int count = 1;
+                // check headers
+
+                while (line != null) {
+                    line = bufferedReader.readLine();
+                    // check record
+                    List<String> fileList = Arrays.asList(line.split("\t"));
+                    Assert.assertEquals(fileList.size(), list.get(count).size());
+                    Assert.assertTrue(list.get(count).containsAll(fileList));
+
+                    count++;
+                }
+            }
+
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 
     private void submitComputeTask(String xml, String drl) {
         ComputeClient client = null;
-        try {
-            String name = "";
-            if (xml.contains("/")) {
-                String[] arr = xml.split("/");
-                name = arr[arr.length - 1].substring(0, arr[arr.length - 1].length() - 4);
-            } else {
-                name = xml.substring(0, xml.length() - 4);
-            }
+        String name = "";
+        if (xml.contains("/")) {
+            String[] arr = xml.split("/");
+            name = arr[arr.length - 1].substring(0, arr[arr.length - 1].length() - 4);
+        } else {
+            name = xml.substring(0, xml.length() - 4);
+        }
 
-            String taskId = "Cucumber-test-" + name + "-" + System.currentTimeMillis();
-            String commandString = "-type F";
-            if (computingPartition == null || computingPartition == 0) {
-                computingPartition = 1;
-            }
+        String taskId = "Cucumber-test-" + name + "-" + System.currentTimeMillis();
+        String commandString = "-type F";
+        if (computingPartition == null || computingPartition == 0) {
+            computingPartition = 1;
+        }
+        try {
+            sendRecord("gdmLocationType.tsv", "START_FILE");
+
             client = new ComputeClient();
             client.connect(computingNode);
             client.submitTask(taskId, xml, drl, commandString, computingPartition);
@@ -84,8 +180,38 @@ public class PangeaSteps extends CommonSteps {
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
+            sendRecord("gdmLocationType.tsv", "END_FILE");
             if (client != null) {
                 client.close();
+            }
+        }
+    }
+
+    private void sendRecord(String fileName, String type) {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, io.confluent.kafka.serializers.KafkaAvroSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, io.confluent.kafka.serializers.KafkaAvroSerializer.class);
+        props.put("schema.registry.url", schemaRegistryUrl);
+        try (KafkaProducer producer = new KafkaProducer(props)){
+            String startKeySchema = "{\"type\":\"record\",\"name\":\"keySchema\",\"fields\":[{\"name\":\"_OP_TYPE_\",\"type\":\"string\"}]}";
+            String gdmFileSchema = "{\"type\":\"record\",\"name\":\"fileSchema\",\"fields\":[{\"name\":\"fileName\",\"type\":\"string\"}]}";
+
+            Schema.Parser parser = new Schema.Parser();
+            Schema schema = parser.parse(startKeySchema);
+            GenericRecord keyRecord = new GenericData.Record(schema);
+            keyRecord.put("_OP_TYPE_", type);
+
+            parser = new Schema.Parser();
+            schema = parser.parse(gdmFileSchema);
+            GenericRecord avroRecord = new GenericData.Record(schema);
+            avroRecord.put("fileName", fileName);
+            ProducerRecord<Object, Object> record = new ProducerRecord<Object, Object>(topic, keyRecord, avroRecord);
+
+            try {
+                producer.send(record);
+            } catch (SerializationException e) {
+                // may need to do something with it
             }
         }
     }
