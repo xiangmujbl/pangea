cb831f28becfad6b0cbb9d26152692cfd1f4208f
QLiu84 <qliu84@its.jnj.com>
2018-05-11 14:01:45 +0800

    AEAZ-1277 update EDMGlobalBaseUnit

--- pangea-parent/pangea-test/src/test/resources/features/edm/EDMGlobalBusinessUnit_ProjectOne.feature
+++ pangea-parent/pangea-test/src/test/resources/features/edm/EDMGlobalBusinessUnit_ProjectOne.feature
@@ -4,17 +4,17 @@ Feature: EDMGlobalBaseUnit AEAZ-1277
 
   Scenario: Full Load curation
 
-    Given I import "/ngems/global_base_unit_v1" by keyFields "gbu"
+    Given I import "/ngems/global_business_unit_v1" by keyFields "gbu"
       | gbu  | gbuName  |
       | 1001 | SKINCARE |
       | 1002 | CHC      |
       | 1003 | OTC      |
 
-    And I wait "/ngems/global_base_unit_v1" Async Queue complete
+    And I wait "/ngems/global_business_unit_v1" Async Queue complete
 
-    When I submit task with xml file "xml/edm/EDMGlobalBaseUnit_ProjectOne.xml" and execute file "jar/pangea-view.jar"
+    When I submit task with xml file "xml/edm/EDMGlobalBusinessUnit_ProjectOne.xml" and execute file "jar/pangea-view.jar"
 
-    Then I check region data "/edm/global_base_unit_v1" by keyFields "gbu"
+    Then I check region data "/edm/global_business_unit_v1" by keyFields "gbu"
       | gbu  | gbuName  |
       | 1001 | SKINCARE |
       | 1002 | CHC      |
@@ -23,9 +23,9 @@ Feature: EDMGlobalBaseUnit AEAZ-1277
     Then I check region data "/plan/edm_failed_data" by keyFields "functionalArea,interfaceID,errorCode,sourceSystem,key1,key2,key3,key4,key5"
       | functionalArea | interfaceID | errorCode | sourceSystem | businessArea | key1 | key2 | key3 | key4 | key5 | errorValue |
 
-    And I compare the number of records between "/ngems/global_base_unit_v1" and "/edm/global_base_unit_v1,/plan/edm_failed_data"
+    And I compare the number of records between "/ngems/global_business_unit_v1" and "/edm/global_business_unit_v1,/plan/edm_failed_data"
 
     And I delete the test data
 
-    And I will remove all data with region "/edm/global_base_unit_v1"
+    And I will remove all data with region "/edm/global_business_unit_v1"
     And I will remove all data with region "/plan/edm_failed_data"
--- pangea-parent/pangea-view/src/main/java/com/jnj/pangea/common/dao/impl/edm/EDMGlobalBusinessUnitV1DaoImpl.java
+++ pangea-parent/pangea-view/src/main/java/com/jnj/pangea/common/dao/impl/edm/EDMGlobalBusinessUnitV1DaoImpl.java
@@ -3,28 +3,28 @@ package com.jnj.pangea.common.dao.impl.edm;
 import com.jnj.pangea.common.dao.impl.CommonDaoImpl;
 import com.jnj.adf.client.api.query.QueryHelper;
 import com.jnj.pangea.common.IConstant;
-import com.jnj.pangea.common.entity.edm.EDMGlobalBaseUnitV1Entity;
+import com.jnj.pangea.common.entity.edm.EDMGlobalBusinessUnitV1Entity;
 
-public class EDMGlobalBaseUnitV1DaoImpl extends CommonDaoImpl {
+public class EDMGlobalBusinessUnitV1DaoImpl extends CommonDaoImpl {
 
-    private static EDMGlobalBaseUnitV1DaoImpl instance;
+    private static EDMGlobalBusinessUnitV1DaoImpl instance;
 
-    public static EDMGlobalBaseUnitV1DaoImpl getInstance() {
+    public static EDMGlobalBusinessUnitV1DaoImpl getInstance() {
         if (instance == null) {
-            instance = new EDMGlobalBaseUnitV1DaoImpl();
+            instance = new EDMGlobalBusinessUnitV1DaoImpl();
         }
         return instance;
     }
 
-    public EDMGlobalBaseUnitV1Entity getEntityWithConditions(String param) {
+    public EDMGlobalBusinessUnitV1Entity getEntityWithConditions(String param) {
 
         //String queryString = QueryHelper.buildCriteria(IConstant.EDM_COUNTRY_V1.LOCAL_COUNTRY).is(land1).toQueryString();
         //return queryForObject(IConstant.REGION.EDM_COUNTRY_V1, localQueryString, EDMCountryEntity.class);
         return null;
     }
 
-    public EDMGlobalBaseUnitV1Entity getEntityWithGbu(String globalBusinessUnit) {
+    public EDMGlobalBusinessUnitV1Entity getEntityWithGbu(String globalBusinessUnit) {
         String queryString = QueryHelper.buildCriteria(IConstant.EDM_GLOBAL_BASE_UNIT_V1.GBU).is(globalBusinessUnit).toQueryString();
-        return queryForObject(IConstant.REGION.EDM_GLOBAL_BASE_UNIT_V1, queryString, EDMGlobalBaseUnitV1Entity.class);
+        return queryForObject(IConstant.REGION.EDM_GLOBAL_BASE_UNIT_V1, queryString, EDMGlobalBusinessUnitV1Entity.class);
     }
 }
