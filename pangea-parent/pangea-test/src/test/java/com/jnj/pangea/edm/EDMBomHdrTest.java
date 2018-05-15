480fd32264df7d752748b6dd91dbc03bddcd5063
NHuang12 <nhuang12@its.jnj.com>
2018-05-15 15:53:42 +0800

    AEAZ-3269 add BomHdr Test

---
+++ pangea-parent/pangea-test/src/test/java/com/jnj/pangea/edm/EDMBomHdrTest.java
@@ -0,0 +1,14 @@
+package com.jnj.pangea.edm;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/test/resources/features/edm/EDMBomHdrV1Latam.feature"},
+        tags = {"@pangea_test"},
+        glue = "com.jnj.pangea.sentence",
+        plugin = {"pretty", "json:target/reports/json/AEAZ-3269 EDMBomHdrV1Latam.json"})
+public class EDMBomHdrTest {
+}
