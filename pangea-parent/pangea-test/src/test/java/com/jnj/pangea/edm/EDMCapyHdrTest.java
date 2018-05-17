5e14577ba267b305c2f8a08a3d25adad578d0998
NHuang12 <nhuang12@its.jnj.com>
2018-05-17 19:51:15 +0800

    AEAZ-4241 add feature,Test,Bo,Controller file

---
+++ pangea-parent/pangea-test/src/test/java/com/jnj/pangea/edm/EDMCapyHdrTest.java
@@ -0,0 +1,14 @@
+package com.jnj.pangea.edm;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/test/resources/features/edm/EDMCapyHdr.feature"},
+        tags = {"@pangea_test"},
+        glue = "com.jnj.pangea.sentence",
+        plugin = {"pretty", "json:target/reports/json/AEAZ-4241 EDMCapyHdr.json"})
+public class EDMCapyHdrTest {
+}
