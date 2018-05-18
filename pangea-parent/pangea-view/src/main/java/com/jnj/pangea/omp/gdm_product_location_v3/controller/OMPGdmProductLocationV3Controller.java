1fd866a01291edad05a5f8d661268a635cba16c8
qzhan112 <540225633@qq.com>
2018-05-18 15:26:08 +0800

    add AEAZ-4068's coding and feature files

---
+++ pangea-parent/pangea-view/src/main/java/com/jnj/pangea/omp/gdm_product_location_v3/controller/OMPGdmProductLocationV3Controller.java
@@ -0,0 +1,51 @@
+package com.jnj.pangea.omp.gdm_product_location_v3.controller;
+
+import com.jnj.adf.client.api.remote.RawDataValue;
+import com.jnj.adf.curation.logic.RawDataEvent;
+import com.jnj.adf.curation.logic.ViewResultBuilder;
+import com.jnj.adf.curation.logic.ViewResultItem;
+import com.jnj.pangea.common.BaseBo;
+import com.jnj.pangea.common.FailData;
+import com.jnj.pangea.common.ResultObject;
+import com.jnj.pangea.common.controller.BaseController;
+import com.jnj.pangea.common.entity.edm.EDMMaterialPlantV1Entity;
+import com.jnj.pangea.omp.gdm_product_location_v3.service.OMPGdmProductLocationV3ServiceImpl;
+import com.jnj.pangea.util.BeanUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OMPGdmProductLocationV3Controller extends BaseController {
+
+    private OMPGdmProductLocationV3ServiceImpl service = OMPGdmProductLocationV3ServiceImpl.getInstance();
+
+    @Override
+    public List<ViewResultItem> process(List<RawDataEvent> list) {
+
+        List<ViewResultItem> result = new ArrayList<>();
+        list.forEach(raw -> {
+
+            RawDataValue rawValue = raw.getValue();
+
+            EDMMaterialPlantV1Entity plantAttrEntity = BeanUtil.mapToBean(rawValue.toMap(), EDMMaterialPlantV1Entity.class);
+
+            List<ResultObject> resultObjectList = service.buildView(raw.getKey(), plantAttrEntity, null);
+
+            for (ResultObject resultObject:resultObjectList) {
+                if (resultObject.isSuccess()) {
+                    BaseBo baseBo = resultObject.getBaseBo();
+                    ViewResultItem viewRaw = ViewResultBuilder.newResultItem(baseBo.getKey(), baseBo.toMap());
+                    result.add(viewRaw);
+                } else {
+                    if (resultObject.getFailData() != null) {
+                        FailData failData = resultObject.getFailData();
+                        ViewResultItem viewResultItem = ViewResultBuilder.newResultItem(failData.getFailRegion(), failData.getKey(), failData.toMap());
+                        result.add(viewResultItem);
+                    }
+                }
+            }
+
+        });
+        return result;
+    }
+}
