5ca1c000ac2d508490e7ac1bc5b0289623d5e368
NHuang12 <nhuang12@its.jnj.com>
2018-05-21 11:38:37 +0800

    SonarLint:update bug

--- pangea-parent/pangea-view/src/main/java/com/jnj/pangea/common/dao/impl/project_one/ProjectOneStkoDaoImpl.java
+++ pangea-parent/pangea-view/src/main/java/com/jnj/pangea/common/dao/impl/project_one/ProjectOneStkoDaoImpl.java
@@ -1,7 +1,6 @@
 package com.jnj.pangea.common.dao.impl.project_one;
 
 import com.jnj.adf.client.api.query.QueryHelper;
-import com.jnj.adf.grid.utils.LogUtil;
 import com.jnj.pangea.common.IConstant;
 import com.jnj.pangea.common.dao.impl.CommonDaoImpl;
 import com.jnj.pangea.common.entity.project_one.StkoEntity;
@@ -23,10 +22,8 @@ public class ProjectOneStkoDaoImpl extends CommonDaoImpl {
         if (StringUtils.isNotBlank(stlnr) && StringUtils.isNotBlank(stlal)){
             String queryString = QueryHelper.buildCriteria(IConstant.PROJECT_ONE_STKO.STLNR).is(stlnr).
                     and(IConstant.PROJECT_ONE_STKO.STLAL).is(stlal).toQueryString();
-            LogUtil.getCoreLog().info("----------queryString---------"+queryString);
-            //return queryForList(IConstant.REGION.PROJECT_ONE_STKO, queryString, StkoEntity.class,IConstant.PROJECT_ONE_STKO.STKOZ);
             return queryForList(IConstant.REGION.PROJECT_ONE_STKO_CLONE, queryString, StkoEntity.class);
         }
-        return null;
+       return null;
     }
 }
