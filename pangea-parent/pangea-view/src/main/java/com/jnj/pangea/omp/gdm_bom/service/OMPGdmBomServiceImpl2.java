6dda07fd12493c9a029a673e6377908f9b3c56df
ccheng40 <ccheng40@its.jnj.com>
2018-06-01 16:30:16 +0800

    AEAZ-4480

--- pangea-parent/pangea-view/src/main/java/com/jnj/pangea/omp/gdm_bom/service/OMPGdmBomServiceImpl2.java
+++ pangea-parent/pangea-view/src/main/java/com/jnj/pangea/omp/gdm_bom/service/OMPGdmBomServiceImpl2.java
@@ -47,46 +47,46 @@ public class OMPGdmBomServiceImpl2 {
         planParameterEntityList.forEach(planCnsPlanParameterEntity -> {
             OMPGdmBomBo gdmBOMBo = new OMPGdmBomBo();
             String parameterValue = planCnsPlanParameterEntity.getParameterValue();
-        if(mfgOrderEntity.getSourceSysCd() .equals(planCnsPlanParameterEntity.getAttribute()) && (current_date == actRlseDt || actRlseDt == current_date_Minus_One)){
+            if(mfgOrderEntity.getSourceSysCd() .equals(planCnsPlanParameterEntity.getAttribute()) && (current_date == actRlseDt || actRlseDt == current_date_Minus_One)){
 //            if(mfgOrderEntity.getSourceSysCd() .equals(planCnsPlanParameterEntity.getAttribute())){
-            List<EDMMfgOrderRtngEntity> edmMfgOrderRtngEntityList = joinMfgOrderRtngWithOrdrRtngNum(mfgOrderEntity.getOrdrRtngNum());
-            edmMfgOrderRtngEntityList.forEach(edmMfgOrderRtngEntity -> {
+                List<EDMMfgOrderRtngEntity> edmMfgOrderRtngEntityList = joinMfgOrderRtngWithOrdrRtngNum(mfgOrderEntity.getOrdrRtngNum());
+                edmMfgOrderRtngEntityList.forEach(edmMfgOrderRtngEntity -> {
 
-                OMPGdmBomBo gdmBOMBo1 = null;
-                try {
-                    gdmBOMBo1 = (OMPGdmBomBo)gdmBOMBo.copy();
-                } catch (CloneNotSupportedException e) {
-                    e.printStackTrace();
-                }
-
-                List<EDMMfgOrderSeqEntity> edmMfgOrderSeqEntityList = joinMfgOrderSeqWithOrdrRtngNum(edmMfgOrderRtngEntity.getOrdrRtngNum() );
-                OMPGdmBomBo finalGdmBOMBo = gdmBOMBo1;
-                edmMfgOrderSeqEntityList.forEach(edmMfgOrderSeqEntity -> {
+                    OMPGdmBomBo gdmBOMBo1 = null;
                     try {
-                        OMPGdmBomBo gdmBOMBo2 = (OMPGdmBomBo)finalGdmBOMBo.copy();
-                        String bomId = "PRO"+"/"+parameterValue+"_"+mfgOrderEntity.getMfgOrdrNum()+"/"+mfgOrderEntity.getOrdrRtngNum()+"/"+edmMfgOrderRtngEntity.getOperNum();
-                        gdmBOMBo2.setBomId(bomId);
-                        gdmBOMBo2.setActive("YES");
-                        gdmBOMBo2.setActiveFCTERP("YES");
-                        gdmBOMBo2.setActiveOPRERP("YES");
-                        gdmBOMBo2.setActiveSOPERP("NO");
-                        gdmBOMBo2.setComments("");
-                        gdmBOMBo2.setStartEff("01/01/1980  00:00:00");
-                        gdmBOMBo2.setEndEff("31/12/2298  23:59:59");
-
-
-                        gdmBOMBo2.setLocationId(mfgOrderEntity.getSourceSysCd()+"_"+mfgOrderEntity.getPlntCd());
-                        ResultObject resultObject = new ResultObject();
-                        resultObject.setBaseBo(gdmBOMBo2);
-                        resultObjectList.add(resultObject);
+                        gdmBOMBo1 = (OMPGdmBomBo)gdmBOMBo.copy();
                     } catch (CloneNotSupportedException e) {
                         e.printStackTrace();
                     }
 
-                });
+                    List<EDMMfgOrderSeqEntity> edmMfgOrderSeqEntityList = joinMfgOrderSeqWithOrdrRtngNum(edmMfgOrderRtngEntity.getOrdrRtngNum() );
+                    OMPGdmBomBo finalGdmBOMBo = gdmBOMBo1;
+                    edmMfgOrderSeqEntityList.forEach(edmMfgOrderSeqEntity -> {
+                        try {
+                            OMPGdmBomBo gdmBOMBo2 = (OMPGdmBomBo)finalGdmBOMBo.copy();
+                            String bomId = "PRO"+"/"+parameterValue+"_"+Integer.valueOf(mfgOrderEntity.getMfgOrdrNum())+"/"+Integer.valueOf(mfgOrderEntity.getOrdrRtngNum())+"/"+Integer.valueOf(edmMfgOrderRtngEntity.getOperNum());
+                            gdmBOMBo2.setBomId(bomId);
+                            gdmBOMBo2.setActive("YES");
+                            gdmBOMBo2.setActiveFCTERP("YES");
+                            gdmBOMBo2.setActiveOPRERP("YES");
+                            gdmBOMBo2.setActiveSOPERP("NO");
+                            gdmBOMBo2.setComments("");
+                            gdmBOMBo2.setStartEff("01/01/1980  00:00:00");
+                            gdmBOMBo2.setEndEff("31/12/2298  23:59:59");
 
-            });
-        }
+
+                            gdmBOMBo2.setLocationId(mfgOrderEntity.getSourceSysCd()+"_"+mfgOrderEntity.getPlntCd());
+                            ResultObject resultObject = new ResultObject();
+                            resultObject.setBaseBo(gdmBOMBo2);
+                            resultObjectList.add(resultObject);
+                        } catch (CloneNotSupportedException e) {
+                            e.printStackTrace();
+                        }
+
+                    });
+
+                });
+            }
         });
         return resultObjectList;
     }
