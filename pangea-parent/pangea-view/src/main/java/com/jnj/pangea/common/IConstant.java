7f88049318145c8cfc03c0d7e3b02a7b40367565
Unknown <pgreg@its.jnj.com>
2018-06-08 13:52:57 +0100

    AEAZ-5870 Progess  on implementation
    Progess o the story

--- pangea-parent/pangea-view/src/main/java/com/jnj/pangea/common/IConstant.java
+++ pangea-parent/pangea-view/src/main/java/com/jnj/pangea/common/IConstant.java
@@ -465,7 +465,7 @@ public interface IConstant {
         String SOURCE_SYSTEM = "sourceSystem";
         String SOURCE_OBJECT_TECHNAME = "sourceObjectTechName";
         String SOURCE_OBJECT_ATTRIBUTE1_VALUE = "sourceObjectAttribute1Value";
-        String SOURCE_OBJECT_PLANT_ATTRIBUTE = "sourceObjectPlantAttribute";
+        String SOURCE_OBJECT_PLANT_ATTRIBUTE1 = "sourceObjectAttribute1";
         String SOURCE_FILTER_PLANT_VALUE = "sourceFilterPlantValue";
     }
 
