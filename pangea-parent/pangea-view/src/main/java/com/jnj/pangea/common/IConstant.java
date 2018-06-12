f6ce74dd73ca6146fa514e4e7f5ad3ba6cc91f90
QLiu84 <qliu84@its.jnj.com>
2018-06-12 18:06:01 +0800

    AEAZ-3215 GDMLfu add annotation

--- pangea-parent/pangea-view/src/main/java/com/jnj/pangea/common/IConstant.java
+++ pangea-parent/pangea-view/src/main/java/com/jnj/pangea/common/IConstant.java
@@ -854,6 +854,8 @@ public interface IConstant {
         String DATA_OBJECT = "dataObject";
         String SPLIT = "-";
         String SPLIT_ = "_";
+
+        //regex expression of time , if it is satisfied, do format.
         String CHCEK_TIME = "^((((1[6-9]|[2-9]\\d)\\d{2})-(0?[13578]|1[02])-(0?[1-9]|[12]\\d|3[01]))|(((1[6-9]|[2-9]\\d)\\d{2})-(0?[13456789]|1[012])-(0?[1-9]|[12]\\d|30))|(((1[6-9]|[2-9]\\d)\\d{2})-0?2-(0?[1-9]|1\\d|2[0-9]))|(((1[6-9]|[2-9]\\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))-0?2-29-))$";
         String PATTERN_DECIMAL_3="#.000";
         String PATTERN_DECIMAL_5="#.00000";
