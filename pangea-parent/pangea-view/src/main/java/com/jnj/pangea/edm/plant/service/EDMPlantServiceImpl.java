95a991c9c2d2361575d5954dbde99745ec3ffd4d
QLiu84 <qliu84@its.jnj.com>
2018-06-15 11:53:24 +0800

    add qa R3

--- pangea-parent/pangea-view/src/main/java/com/jnj/pangea/edm/plant/service/EDMPlantServiceImpl.java
+++ pangea-parent/pangea-view/src/main/java/com/jnj/pangea/edm/plant/service/EDMPlantServiceImpl.java
@@ -1,12 +1,16 @@
 package com.jnj.pangea.edm.plant.service;
 
+import com.jnj.pangea.common.FailData;
+import com.jnj.pangea.common.IConstant;
 import com.jnj.pangea.common.ResultObject;
 import com.jnj.pangea.common.dao.impl.edm.EDMCountryV1DaoImpl;
+import com.jnj.pangea.common.dao.impl.edm.EDMPlantInputDaoImpl;
 import com.jnj.pangea.common.dao.impl.edm.EDMSourceSystemV1DaoImpl;
 import com.jnj.pangea.common.dao.impl.project_one.ProjectOneT001DaoImpl;
 import com.jnj.pangea.common.dao.impl.project_one.ProjectOneT001KDaoImpl;
 import com.jnj.pangea.common.dao.impl.project_one.ProjectOneT001WDaoImpl;
 import com.jnj.pangea.common.entity.edm.EDMCountryEntity;
+import com.jnj.pangea.common.entity.edm.EDMPlantInputEntity;
 import com.jnj.pangea.common.entity.edm.EDMSourceSystemV1Entity;
 import com.jnj.pangea.common.entity.ems.EMSFZEnterprisePlants;
 import com.jnj.pangea.common.entity.project_one.T001Entity;
@@ -32,6 +36,7 @@ public class EDMPlantServiceImpl implements ICommonService {
     private EDMCountryV1DaoImpl countryV1Dao = EDMCountryV1DaoImpl.getInstance();
     private ProjectOneT001KDaoImpl t001KDao = ProjectOneT001KDaoImpl.getInstance();
     private ProjectOneT001DaoImpl t001Dao = ProjectOneT001DaoImpl.getInstance();
+    private EDMPlantInputDaoImpl plantInputDao = EDMPlantInputDaoImpl.getInstance();
 
     @Override
     public ResultObject buildView(String key, Object o, Object o2) {
@@ -45,9 +50,18 @@ public class EDMPlantServiceImpl implements ICommonService {
         // rule T1
         String zPlantSourceSystem = enterprisePlants.getzPlantSourceSystem();
 
-        plantBo.setSourceSystem(getFieldWithT1(zPlantSourceSystem));
 
-        String localPlant = enterprisePlants.getzPlant().split(",")[1].trim();
+        String sourceSystem=getFieldWithT1(zPlantSourceSystem);
+        if(StringUtils.isBlank ( sourceSystem )){
+            return resultObject;
+        }
+
+        plantBo.setSourceSystem(sourceSystem);
+        String zPalnt=enterprisePlants.getzPlant();
+        if(StringUtils.isBlank ( zPalnt )||zPalnt.trim ().split ( IConstant.EDM_PLANT_V1.SPLIT_ ).length<2){
+            return resultObject;
+        }
+        String localPlant = zPalnt.split(IConstant.EDM_PLANT_V1.SPLIT_)[1].trim();
         plantBo.setLocalPlant(localPlant);
 
         // rule T2
@@ -62,10 +76,24 @@ public class EDMPlantServiceImpl implements ICommonService {
             plantBo.setCountry(getFieldWithT4(LocalCountry));
             plantBo.setLocalPlantType(t001WEntity.getNodetype());
             // rule J1
-            plantBo.setLocalCurrency(getFieldWithJ1(t001WEntity.getBwkey()));
+            T001Entity  t001Entity= getFieldWithJ1(t001WEntity.getBwkey());
+            if(t001Entity==null){
+                return resultObject;
+            }
+            plantBo.setLocalCurrency(t001Entity.getWaers ());
+        }else {
+            return resultObject;
         }
+
         plantBo.setPlant(enterprisePlants.getzEntPlantNumber());
-        plantBo.setLocalPlanningRelevant("");
+        // rule J2
+
+        EDMPlantInputEntity edmPlantInputEntity = getFieldWithJ2(plantBo.getSourceSystem(), localPlant);
+        if (null != edmPlantInputEntity) {
+            plantBo.setLocalPlanningRelevant(edmPlantInputEntity.getLocalPlanningRelevant());
+        } else {
+            plantBo.setLocalPlanningRelevant(IConstant.EDM_PLANT_V1.FIRLD_BLANK);
+        }
         plantBo.setSite(enterprisePlants.getzSite());
         plantBo.setPlantType(enterprisePlants.getzEntPlantType());
         plantBo.setRegion(enterprisePlants.getzRegion());
@@ -78,13 +106,13 @@ public class EDMPlantServiceImpl implements ICommonService {
     private String getFieldWithT1(String zPlantSourceSystem) {
 
         if (StringUtils.isEmpty(zPlantSourceSystem)) {
-            return "";
+            return IConstant.EDM_PLANT_V1.FIRLD_BLANK;
         }
         EDMSourceSystemV1Entity sourceSystemV1Entry = sourceSystemV1Dao.getEntityWithLocalSourceSystem(zPlantSourceSystem);
         if (null != sourceSystemV1Entry) {
             return sourceSystemV1Entry.getSourceSystem();
         }
-        return "";
+        return IConstant.EDM_PLANT_V1.FIRLD_BLANK;
     }
 
     private T001WEntity getFieldsWithT2(String zPlant) {
@@ -98,34 +126,47 @@ public class EDMPlantServiceImpl implements ICommonService {
     private String getFieldWithT4(String land1) {
 
         if (StringUtils.isEmpty(land1)) {
-            return "";
+            return IConstant.EDM_PLANT_V1.FIRLD_BLANK;
         }
         EDMCountryEntity countryEntity = countryV1Dao.getEntityWithLocalCountry(land1);
         if (null != countryEntity) {
             return countryEntity.getCountryCode();
         }
-        return "";
+        return IConstant.EDM_PLANT_V1.FIRLD_BLANK;
     }
 
-    private String getFieldWithJ1(String bwkey) {
-
-        if (StringUtils.isEmpty(bwkey)) {
-            return "";
-        }
+    private T001Entity getFieldWithJ1(String bwkey) {
         T001KEntity t001KEntity = t001KDao.getEntityWithBwkey(bwkey);
         if (null != t001KEntity) {
-
             String bukrs = t001KEntity.getBukrs();
-            if (StringUtils.isEmpty(bukrs)) {
-                return "";
-            }
+            return t001Dao.getEntityWithBukrs(bukrs);
 
-            T001Entity t001Entity = t001Dao.getEntityWithBukrs(bukrs);
-            if (null != t001Entity) {
-                return t001Entity.getWaers();
-            }
+        }else{
+            return null;
         }
-        return "";
+    }
+
+    private EDMPlantInputEntity getFieldWithJ2(String SourceSystem, String localPlant) {
+
+        if (StringUtils.isEmpty(localPlant)) {
+            return null;
+        }
+        return plantInputDao.getPlantWithSourceSystemAndLocalPlant(SourceSystem, localPlant);
+    }
+
+    private FailData writeFailDataToRegion(EMSFZEnterprisePlants enterprisePlants, String ruleCode, String errorValue) {
+        FailData failData = new FailData();
+        failData.setFunctionalArea(IConstant.FAILED.FUNCTIONAL_AREA.DP);
+        failData.setInterfaceID(IConstant.FAILED.INTERFACE_ID.EDM_PLANT);
+        failData.setErrorCode(ruleCode);
+        failData.setSourceSystem(enterprisePlants.getzPlantSourceSystem());
+        failData.setKey1(enterprisePlants.getzPlant());
+        failData.setKey2(IConstant.EDM_PLANT_V1.FIRLD_BLANK);
+        failData.setKey3(IConstant.EDM_PLANT_V1.FIRLD_BLANK);
+        failData.setKey4(IConstant.EDM_PLANT_V1.FIRLD_BLANK);
+        failData.setKey5(IConstant.EDM_PLANT_V1.FIRLD_BLANK);
+        failData.setErrorValue(errorValue);
+        return failData;
     }
 
 }
