9cdcb94c336e45a4f4c329876cbf746d4cbfee33
RShah57 <RShah57@DESKTOP-3JAS3JC.wks.jnj.com>
2018-07-02 16:15:29 -0400

    AEAZ-5744: Made the change from demandGroup to customerId for the region cns_productcustomer

--- pangea-parent/pangea-view/src/main/java/com/jnj/pangea/common/entity/plan/PlanCnsProductCustomerEntity.java
+++ pangea-parent/pangea-view/src/main/java/com/jnj/pangea/common/entity/plan/PlanCnsProductCustomerEntity.java
@@ -7,145 +7,39 @@ import java.util.Map;
 public class PlanCnsProductCustomerEntity extends CommonEntity {
 
     private String sourceSystem;
-    private String customerId;
+    private String demandGroup;
     private String productId;
-    private String productStatus;
-    private String moq;
-    private String ioq;
-    private String stockLevel;
-    private String uom;
-    private String locationId;
-    private String norm;
-    private String leadTime;
-    private String roundingThreshold;
 
     public PlanCnsProductCustomerEntity(Map<String, Object> map) {
         super(map);
+
         setSourceSystem((String) map.get("sourceSystem"));
-        setCustomerId((String) map.get("customerId"));
+        setDemandGroup((String) map.get("demandGroup"));
         setProductId((String) map.get("productId"));
-        setProductStatus((String) map.get("productStatus"));
-        setMoq((String) map.get("moq"));
-        setIoq((String) map.get("ioq"));
-        setStockLevel((String) map.get("stockLevel"));
-        setUom((String) map.get("uom"));
-        setLocationId((String) map.get("locationId"));
-        setNorm((String) map.get("norm"));
-        setLeadTime((String) map.get("leadTime"));
-        setRoundingThreshold((String) map.get("roundingThreshold"));
     }
 
     public String getSourceSystem() {
-        return sourceSystem;
+        return this.sourceSystem;
     }
 
     public void setSourceSystem(String sourceSystem) {
         this.sourceSystem = sourceSystem;
     }
 
-    public String getCustomerId() {
-        return customerId;
+    public String getDemandGroup() {
+        return this.demandGroup;
     }
 
-    public void setCustomerId(String customerId) {
-        this.customerId = customerId;
+    public void setDemandGroup(String demandGroup) {
+        this.demandGroup = demandGroup;
     }
 
     public String getProductId() {
-        return productId;
+        return this.productId;
     }
 
     public void setProductId(String productId) {
         this.productId = productId;
     }
 
-    public String getProductStatus() {
-        return productStatus;
-    }
-
-    public void setProductStatus(String productStatus) {
-        this.productStatus = productStatus;
-    }
-
-    public String getMoq() {
-        return moq;
-    }
-
-    public void setMoq(String moq) {
-        this.moq = moq;
-    }
-
-    public String getIoq() {
-        return ioq;
-    }
-
-    public void setIoq(String ioq) {
-        this.ioq = ioq;
-    }
-
-    public String getStockLevel() {
-        return stockLevel;
-    }
-
-    public void setStockLevel(String stockLevel) {
-        this.stockLevel = stockLevel;
-    }
-
-    public String getUom() {
-        return uom;
-    }
-
-    public void setUom(String uom) {
-        this.uom = uom;
-    }
-
-    public String getLocationId() {
-        return locationId;
-    }
-
-    public void setLocationId(String locationId) {
-        this.locationId = locationId;
-    }
-
-    public String getNorm() {
-        return norm;
-    }
-
-    public void setNorm(String norm) {
-        this.norm = norm;
-    }
-
-    public String getLeadTime() {
-        return leadTime;
-    }
-
-    public void setLeadTime(String leadTime) {
-        this.leadTime = leadTime;
-    }
-
-    public String getRoundingThreshold() {
-        return roundingThreshold;
-    }
-
-    public void setRoundingThreshold(String roundingThreshold) {
-        this.roundingThreshold = roundingThreshold;
-    }
-
-    @Override
-    public String toString() {
-        return "PlanCnsProductCustomerEntity{" +
-                "sourceSystem='" + sourceSystem + '\'' +
-                ", customerId='" + customerId + '\'' +
-                ", productId='" + productId + '\'' +
-                ", productStatus='" + productStatus + '\'' +
-                ", moq='" + moq + '\'' +
-                ", ioq='" + ioq + '\'' +
-                ", stockLevel='" + stockLevel + '\'' +
-                ", uom='" + uom + '\'' +
-                ", locationId='" + locationId + '\'' +
-                ", norm='" + norm + '\'' +
-                ", leadTime='" + leadTime + '\'' +
-                ", roundingThreshold='" + roundingThreshold + '\'' +
-                '}';
-    }
 }
