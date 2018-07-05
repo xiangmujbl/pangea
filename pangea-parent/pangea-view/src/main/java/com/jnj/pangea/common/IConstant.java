7e420c04d5f3626a391c1cd1662a632a0d5fbb4e
qzhan112 <540225633@qq.com>
2018-07-05 11:31:24 +0800

    AEAZ-6661 resolve conflits

--- pangea-parent/pangea-view/src/main/java/com/jnj/pangea/common/IConstant.java
+++ pangea-parent/pangea-view/src/main/java/com/jnj/pangea/common/IConstant.java
@@ -8,7 +8,7 @@ public interface IConstant {
     interface REGION {
 
         // -----region
-        String EDM_CAPY_HDR = "/edm/capy_hdr";
+        String EDM_CAPY_HDR = "/edm/capy_hdr/edm/capy_hdr";
         String EDM_WRK_CTR = "/edm/wrk_ctr";
         String PROJECT_ONE_Qave = "/project_one/qave";
         String EDM_SOURCE_SYSTEM_V1 = "/edm/source_system_v1";
@@ -46,6 +46,8 @@ public interface IConstant {
         String PROJECT_ONE_VBAP = "/project_one/vbap";
         String PROJECT_ONE_VBPA = "/project_one/vbpa";
         String PROJECT_ONE_VBKD = "/project_one/vbkd";
+        String PROJECT_ONE_VBUK = "/project_one/vbuk";
+        String PROJECT_ONE_VBUP = "/project_one/vbup";
         String CNS_PLAN_UNIT = "/plan/cns_plan_unit";
         String EDM_MATERIAL_AUOM_V1 = "/edm/material_auom_v1";
         String PLAN_CNS_PROD_LOC_ATTRIB = "/plan/cns_prod_loc_attrib";
@@ -446,6 +448,17 @@ public interface IConstant {
         String VBELN = "vbeln";
         String POSNR = "posnr";
         String PARVW = "parvw";
+    }
+
+    interface PROJECT_ONE_VBUK {
+        String VBELN = "vbeln";
+        String MANDT = "mandt";
+    }
+
+    interface PROJECT_ONE_VBUP {
+        String VBELN = "vbeln";
+        String POSNR = "posnr";
+        String MANDT = "mandt";
     }
 
     interface NGEMS_MATERIAL_LINKAGE {
@@ -1105,7 +1118,6 @@ public interface IConstant {
         String STARTEFF_HMS = "1980/01/01 00:00:00";
         String HHMMSS = " 23:59:59";
         String HHMMSSSTART =" 00:00:00";
-
     }
 
     interface FAILED {
