b72b960f41a5ed4261dda81d05ac583d282a9073
qzhan112 <540225633@qq.com>
2018-07-09 16:04:52 +0800

    AEAZ-7853 add code and feature

--- pangea-parent/pangea-view/src/main/java/com/jnj/pangea/common/IConstant.java
+++ pangea-parent/pangea-view/src/main/java/com/jnj/pangea/common/IConstant.java
@@ -834,7 +834,7 @@ public interface IConstant {
 
     interface MATL_MFG_RTNG {
         String FIELD_LOEKZ_VALUE_X = "x";
-        String FIELD_MATLRTNGVALID_TO = "9999/12/31";
+        String FIELD_MATLRTNGVALID_TO = "99991231";
         String FIELD_NAME_PLNNR = "plnnr";
         String FIELD_NAME_PLNTY = "plnty";
         String FIELD_NAME_PLNAL = "plnal";
@@ -852,7 +852,7 @@ public interface IConstant {
 
     interface BOM_ITEM {
         String FIELD_STAS_STLTY_VALUE = "stlty";
-        String FIELD_MATLRTNGVALID_TO = "9999/12/31";
+        String FIELD_MATLRTNGVALID_TO = "99991231";
         String FIELD_STAS_STLNR_VALUE = "stlnr";
         String FIELD_STAS_STLKN_VALUE = "stlkn";
         String FIELD_STAS_STASZ_VALUE = "stasz";
@@ -870,7 +870,7 @@ public interface IConstant {
 
     interface MFG_RTNG_RLTNSHP {
         String FIELD_LOEKZ_VALUE_X = "x";
-        String FIELD_MATLRTNGVALID_TO = "9999/12/31";
+        String FIELD_MATLRTNGVALID_TO = "99991231";
         String FIELD_NAME_PLNNR = "plnnr";
         String FIELD_NAME_PLNTY = "plnty";
         String FIELD_NAME_PLNAL = "plnal";
@@ -884,7 +884,7 @@ public interface IConstant {
 
     interface MFG_RTNG_HDR {
         String FIELD_LOEKZ_VALUE_X = "x";
-        String FIELD_MATLRTNGVALID_TO = "9999/12/31";
+        String FIELD_MATLRTNGVALID_TO = "99991231";
         String FIELD_NAME_PLNNR = "plnnr";
         String FIELD_NAME_PLNTY = "plnty";
         String FIELD_NAME_PLNAL = "plnal";
