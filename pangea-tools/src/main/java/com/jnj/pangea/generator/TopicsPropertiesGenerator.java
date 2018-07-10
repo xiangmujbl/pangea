a0643f45833d429b8b56b8b2f4265d2e04183d7a
KDing5 <kding5@its.jnj.com>
2018-07-10 15:10:14 +0800

    AEAZ-6823 add topic generate tools

---
+++ pangea-tools/src/main/java/com/jnj/pangea/generator/TopicsPropertiesGenerator.java
@@ -0,0 +1,35 @@
+package com.jnj.pangea.generator;
+
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * Created by KDing5 on 2018/7/10 14:59(GMT+8).
+ */
+public class TopicsPropertiesGenerator {
+
+    public static void main(String[] args) throws Exception {
+
+        Properties properties = new Properties();
+        properties.load(new FileInputStream("pangea-parent\\pangea-test\\src\\main\\installation\\topic_maps.properties"));
+
+        List<String> topics = new ArrayList<>();
+        List<String> topicsMaps = new ArrayList<>();
+        List<String> topicsLocation = new ArrayList<>();
+
+        properties.forEach((key, value) -> {
+
+            String topicName = (String) key;
+            String fileName = (String) value;
+            topics.add(topicName);
+            topicsMaps.add(topicName + ":'" + fileName + "'");
+            topicsLocation.add(topicName + ":'${static.file.location}'");
+        });
+
+        System.out.println("\r\ntopics=" + String.join(",", topics));
+        System.out.println("\r\nfilenamePrefix.map=" + String.join(",", topicsMaps));
+        System.out.println("\r\nfilePath.map=" + String.join(",", topicsLocation));
+    }
+}
