df8565544fa2f1d8cedb814e99dfee3aa42d4862
ccheng40 <ccheng40@its.jnj.com>
2018-07-12 12:11:45 +0800

    AEAZ-7231

--- pangea-parent/pangea-test/src/test/java/com/jnj/pangea/edm/EDMOutboundDeliveryLineTest.java
+++ pangea-parent/pangea-test/src/test/java/com/jnj/pangea/edm/EDMOutboundDeliveryLineTest.java
@@ -9,6 +9,6 @@ import org.junit.runner.RunWith;
         features = {"src/test/resources/features/edm/EDMOutboundDeliveryLine.feature"},
         tags = {"@pangea_test"},
         glue = "com.jnj.pangea.sentence",
-        plugin = {"pretty", "json:target/reports/json/AEAZ-4690 EDMOutboundDeliveryLine.json"})
+        plugin = {"pretty", "json:target/reports/json/AEAZ-4690 AEAZ-7231 EDMOutboundDeliveryLine.json"})
 public class EDMOutboundDeliveryLineTest {
 }
--- pangea-parent/pangea-view/src/main/java/com/jnj/pangea/common/dao/impl/project_one/VbupDaoImpl.java
+++ pangea-parent/pangea-view/src/main/java/com/jnj/pangea/common/dao/impl/project_one/VbupDaoImpl.java
@@ -1,9 +1,13 @@
 package com.jnj.pangea.common.dao.impl.project_one;
 
+import com.jnj.pangea.common.dao.impl.CommonDaoImpl;
 import com.jnj.adf.client.api.query.QueryHelper;
 import com.jnj.pangea.common.IConstant;
-import com.jnj.pangea.common.dao.impl.CommonDaoImpl;
 import com.jnj.pangea.common.entity.project_one.VbupEntity;
+
+import java.util.List;
+
+import java.util.List;
 
 public class VbupDaoImpl extends CommonDaoImpl {
 
@@ -16,8 +20,12 @@ public class VbupDaoImpl extends CommonDaoImpl {
         return instance;
     }
 
-    public VbupEntity getEntityWithVbelnAndPosnrAndMandt(String vbeln, String posnr, String mandt) {
-        String queryString = QueryHelper.buildCriteria(IConstant.PROJECT_ONE_VBUP.VBELN).is(vbeln).and(IConstant.PROJECT_ONE_VBUP.POSNR).is(posnr).and(IConstant.PROJECT_ONE_VBUP.MANDT).is(mandt).toQueryString();
-        return queryForObject(IConstant.REGION.PROJECT_ONE_VBUP, queryString, VbupEntity.class);
+    public List<VbupEntity> getEntityWithMandtAndVbelnAndPosnr(String mandt, String vbeln, String posnr) {
+
+        String queryString = QueryHelper.buildCriteria(IConstant.PROJECT_ONE_VBUP.MANDT).is(mandt)
+                .and(IConstant.PROJECT_ONE_VBUP.POSNR).is(posnr)
+                .and(IConstant.PROJECT_ONE_VBUP.VBELN).is(vbeln)
+                .toQueryString();
+        return queryForList(IConstant.REGION.PROJECT_ONE_VBUP, queryString, VbupEntity.class);
     }
 }
