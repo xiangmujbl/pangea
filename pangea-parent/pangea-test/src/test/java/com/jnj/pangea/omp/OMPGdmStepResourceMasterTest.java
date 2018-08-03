3982b56e5d53c0d09abddcb239c30c427df3f15c
JLiu315 <JLiu315@its.jnj.com>
2018-08-03 14:45:26 +0800

    AEAZ-9092 update test

---
+++ pangea-parent/pangea-test/src/test/java/com/jnj/pangea/omp/OMPGdmStepResourceMasterTest.java
@@ -0,0 +1,15 @@
+package com.jnj.pangea.omp;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/test/resources/features/omp/OMPGdmStepResourceMaster.feature"},
+        tags = {"@Scenario1"},
+        glue = "com.jnj.pangea.sentence",
+        plugin = {"pretty", "json:target/reports/json/AEAZ-9092 OMPGdmStepResourceMaster.json"})
+public class OMPGdmStepResourceMasterTest {
+}
+
